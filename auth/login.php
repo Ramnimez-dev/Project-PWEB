e521f0b96eccd36dd71da94346673cd336c1aea4
Vendra van Basten <fransiskusadvendra.2024@gmail.com>
2026-08-11 13:48:54 +0700

    p

--- auth/login.php
+++ auth/login.php
@@ -65,25 +65,57 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <h1>Login</h1>
-    <p>Silahkan Isi dengan Benar</p>
+    <div class="container">
 
-    <form action="" method="post">
-        <label for="nomor_induk">Nomor Induk</label>
-        <input type="text" name="nomor_induk" id="nomor_induk" placeholder="Masukkan Nomor Induk">
+        <div class="sidebar">
+            <div class="logo">
+                <img src="" alt="">
+            </div>
+            <div class="brand-text">
+                <h1>
+                    Halo<br>
+                    <span>SarPras!</span>
+                </h1>
+            </div>
+            <div class="line"><hr></div>
+            <p class="description">
+                Sistem informasi sarana
+                dan prasarana sekolah
+                yang terintegrasi
+                dan mudah
+                digunakan.
+            </p>
+        </div>
 
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" placeholder="Masukkan Username">
-
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" placeholder="Masukkan Password">
-
-        <button type="submit">Login</button>
-    </form>
-
-    <p>Belum Punya Akun? <a href="register.php">Register</a></p>
-    </form>
+        <div class="content">
+            <div class="login-wrapper">
+                <div class="header-login">
+                    <h1>Login</h1>
+                    <p>Silahkan Isi dengan Benar</p>
+                </div>
+                <div class="card-login">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="nomor_induk">Nomor Induk</label>
+                            <input type="text" name="nomor_induk" id="nomor_induk" placeholder="Masukkan Nomor Induk">
+                        </div>
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="text" name="username" id="username" placeholder="Masukkan Username">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" id="password" placeholder="Masukkan Password">
+                        </div>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+                <p>Belum Punya Akun? <a href="register.php">Register</a></p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
